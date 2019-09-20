93682c6fceae1619f912b2e00f0b95bca7ace2e8
Cryptophobia <aouzounov@gmail.com>
2019-09-20 14:16:10 -0400

    ref(apis): deprecated apis in k8s 1.16

--- rootfs/scheduler/resources/deployment.py
+++ rootfs/scheduler/resources/deployment.py
@@ -13,8 +13,10 @@ class Deployment(Resource):
 
     @property
     def api_version(self):
+        # API locations have changed since 1.9 and deprecated in 1.16
+        # https://kubernetes.io/blog/2019/07/18/api-deprecations-in-1-16/
         if self.version() >= parse("1.9.0"):
-            return 'extensions/v1beta1'
+            return 'apps/v1'
 
         return 'extensions/v1beta1'
 
