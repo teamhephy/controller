537d74c5f6d6a9ee62dc6e22048e572233197470
Dennis Walters <pooster@gmail.com>
2021-04-06 04:34:37 +0000

    Calculate the sso provider hostname

--- rootfs/api/__init__.py
+++ rootfs/api/__init__.py
@@ -2,4 +2,9 @@
 The **api** Django app presents a RESTful web API for interacting with the **deis** system.
 """
 
+import os
+from urllib.parse import urlparse
+
 __version__ = '2.3.0'
+
+__sso_provider__ = urlparse(os.getenv('OIDC_AUTH_ENDPOINT') or 'https://keycloak.mothership.ey.io/').netloc
